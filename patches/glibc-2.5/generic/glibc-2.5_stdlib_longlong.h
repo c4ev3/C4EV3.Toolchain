b33c4447d073350c7291951dda75141effbde27a
Marc Kleine-Budde <mkl@pengutronix.de>
2008-04-28 16:05:01 +0000

    git-svn-id: https://svn.pengutronix.de/svn/oselas/toolchain/branches/OSELAS.Toolchain-mkl@6243 f8d472c7-5700-0410-ac5a-87979cec3adf

--- patches/glibc-2.5/generic/glibc-2.5_stdlib_longlong.h
+++ patches/glibc-2.5/generic/glibc-2.5_stdlib_longlong.h
@@ -1,5 +1,3 @@
-Index: glibc-2.5/stdlib/longlong.h
-===================================================================
 --- glibc-2.5.orig/stdlib/longlong.h
 +++ glibc-2.5/stdlib/longlong.h
 @@ -205,6 +205,14 @@ UDItype __umulsidi3 (USItype, USItype);
@@ -33,30 +31,3 @@ Index: glibc-2.5/stdlib/longlong.h
  #if defined (__hppa) && W_TYPE_SIZE == 32
 Index: glibc-2.5/ports/sysdeps/arm/mp_clz_tab.c
 ===================================================================
---- /dev/null
-+++ glibc-2.5/ports/sysdeps/arm/mp_clz_tab.c
-@@ -0,0 +1,24 @@
-+/* __clz_tab -- support for longlong.h
-+   Copyright (C) 2004 Free Software Foundation, Inc.
-+   This file is part of the GNU C Library.
-+
-+   The GNU C Library is free software; you can redistribute it and/or
-+   modify it under the terms of the GNU Lesser General Public
-+   License as published by the Free Software Foundation; either
-+   version 2.1 of the License, or (at your option) any later version.
-+
-+   The GNU C Library is distributed in the hope that it will be useful,
-+   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-+   Lesser General Public License for more details.
-+
-+   You should have received a copy of the GNU Lesser General Public
-+   License along with the GNU C Library; if not, write to the Free
-+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-+   02111-1307 USA.  */
-+
-+#if defined(__ARM_ARCH_5__) || defined(__ARM_ARCH_5T__) || defined(__ARM_ARCH_5TE__)
-+/* Nothing required.  */
-+#else
-+#include <stdlib/mp_clz_tab.c>
-+#endif
