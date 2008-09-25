a0b93809a50d4715922f71833ff5273e939cbe7d
Marc Kleine-Budde <mkl@pengutronix.de>
2008-09-25 21:31:30 +0000

    git-svn-id: https://svn.pengutronix.de/svn/oselas/toolchain/trunks/OSELAS.Toolchain-trunk@7595 f8d472c7-5700-0410-ac5a-87979cec3adf

--- build_all_v2.mk
+++ build_all_v2.mk
@@ -14,6 +14,8 @@ SHELL		:= /bin/bash
 PTX_AUTOBUILD_DESTDIR	:= ${PWD}/inst
 export PTX_AUTOBUILD_DESTDIR
 
+BENICE			:= true
+
 #
 BUILDDATE	:= $(shell date +%y%m%d-%H%M)
 SUBVERSIONREV	:= $(strip $(shell svnversion))
@@ -30,18 +32,33 @@ ifeq ($(ARCH),)
 $(error failed to detect arch, or arch is unsupported)
 endif
 
+PTXDIST			:= ./p
+
+ifdef BENICE
+NICE			+= nice -20
+endif
 
 SUFFIX_BUILDTIME	:= build
 SUFFIX_BUILDSTATUS	:= status
 SUFFIX_BUILDREVISION	:= svnrev
 
 CONFIGDIR	:= ptxconfigs
-#CONFIGFILES	:= $(wildcard $(CONFIGDIR)/*.ptxconfig) $(wildcard $(CONFIGDIR)/*/*.ptxconfig)
-CONFIGFILES	:= ptxconfigs/arm-1136jfs-linux-gnueabi_gcc-4.3.2_glibc-2.8_binutils-2.18_kernel-2.6.26-sanitized.ptxconfig
-CONFIGS		:= $(basename $(notdir $(CONFIGFILES)))
-CONFIGS_OUT	:= $(subst _,-,$(CONFIGS))
+CONFIGFILES	:= $(wildcard $(CONFIGDIR)/*.ptxconfig) $(wildcard $(CONFIGDIR)/*/*.ptxconfig)
+#CONFIGFILES	:= ptxconfigs/powerpc-603e-linux-gnu_gcc-4.1.2_glibc-2.5_binutils-2.17_kernel-2.6.18.ptxconfig
+CONFIGS		:= $(notdir $(basename $(CONFIGFILES)))
+CONFIGS_	:= $(subst _,-,$(CONFIGS))
 
-$(eval $(foreach config,$(CONFIGS),OUT2CONFIG_$(subst _,-,$(config)) := $(config)))
+define gen_2config
+$(eval 2CONFIG_$(subst _,-,$(1)) := $(1))
+endef
+
+define gen_2configfile
+$(eval 2CONFIGFILE_$(subst _,-,$(notdir $(basename $(1)))) := $(1))
+endef
+
+$(foreach cfg,$(CONFIGS),$(call gen_2config,$(cfg)))
+$(foreach cfgfile,$(CONFIGFILES),$(call gen_2configfile,$(cfgfile)))
+
 
 STATEDIR	:= gstate
 STATEFILES	:= $(addprefix $(STATEDIR)/,$(addsuffix .$(suffix_buildtime),$(CONFIGS)))
@@ -55,26 +72,28 @@ DISTDIR		:= dist
 
 
 #DEB  = oselas.toolchain-1.1.1-arm-1136jfs-linux-gnueabi-gcc-4.1.2-glibc-2.5-kernel-2.6.18_1.1.1_amd64.deb
-#       oselas.toolchain-     -arm-v4t-linux-gnueabi-    gcc-4.3.2-glibc-2.8-linux-2.6.26-sanitized_amd64.deb
 #TBZ2 = OSELAS.Toolchain-1.1.1-arm-1136jfs-linux-gnueabi-gcc-4.1.2-glibc-2.5-kernel-2.6.18_amd64.tar.bz2
 
+OUT_PREFIX	:= $(foreach config,$(CONFIGS_),$(addprefix $(DISTDIR)/oselas.toolchain-$(VERSION)-,$(config)))
 
-OUT_PREFIX	:= $(foreach config,$(CONFIGS_OUT),$(addprefix $(DISTDIR)/oselas.toolchain-$(VERSION)-,$(config)))
-DEBS		:= $(foreach config,$(OUT_PREFIX),$(addsuffix _$(VERSION)_$(ARCH).deb,$(config)))
-TBZ2S		:= $(foreach config,$(OUT_PREFIX),$(addsuffix _$(ARCH).tar.bz2,$(config)))
+DEB_SUFFIX	:= _$(VERSION)_$(ARCH).deb
+TBZ2_SUFFIX	:= _$(ARCH).tar.bz2
+
+DEBS		:= $(foreach config,$(OUT_PREFIX),$(addsuffix $(DEB_SUFFIX),$(config)))
+TBZ2S		:= $(foreach config,$(OUT_PREFIX),$(addsuffix $(TBZ2_SUFFIX),$(config)))
 
 all: $(TBZ2S)
 
-$(DISTDIR)/%_$(ARCH).deb: $(STATEDIR)/%.build | mkdirs
+$(DISTDIR)/%$(DEB_SUFFIX): $(STATEDIR)/%.build | mkdirs
 	@true
 
-$(DISTDIR)/%_$(ARCH).tar.bz2: $(STATEDIR)/%.build | mkdirs
+$(DISTDIR)/%$(TBZ2_SUFFIX): $(STATEDIR)/%.build | mkdirs
 	@true
 
 $(STATEDIR)/oselas.toolchain-$(VERSION)-%.build: | mkdirs
-	@ptxdist distclean
-	@ptxdist select $(filter %/$(OUT2CONFIG_$(*)).ptxconfig, $(CONFIGFILES))
-	@ptxdist go
+	@$(PTXDIST) distclean
+	@$(PTXDIST) select $(2CONFIGFILE_$(*))
+	@$(NICE) $(PTXDIST) go
 
 mkdirs:
 	@mkdir -p $(STATEDIR) $(DISTDIR)
