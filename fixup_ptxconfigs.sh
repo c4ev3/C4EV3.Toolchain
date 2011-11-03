ca469bcb17d8375ff80859c43895cd3c8ede696e
Michael Olbrich <m.olbrich@pengutronix.de>
2011-11-03 12:34:18 +0100

    fixup_ptxconfigs.sh: use PTXdist 2011.11.0

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -19,7 +19,7 @@ fixup()
     local config="${1}"
 
     # version
-    PTXCONF_CONFIGFILE_VERSION="2011.06.0"
+    PTXCONF_CONFIGFILE_VERSION="2011.11.0"
     PTXCONF_PROJECT="OSELAS.Toolchain-2011.03.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
