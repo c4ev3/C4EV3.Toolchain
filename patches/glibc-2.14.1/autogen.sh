57da51d93a1e096b1b0b45a0fd95d602af62e17d
Marc Kleine-Budde <mkl@pengutronix.de>
2012-09-18 15:49:50 +0200

    glibc-2.14.1: autogen.sh: make configure writeable
    On some systems the configure script it not writeable, and the autoconf command
    will fail otherwise.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- patches/glibc-2.14.1/autogen.sh
+++ patches/glibc-2.14.1/autogen.sh
@@ -4,5 +4,6 @@
 # 0003-configure.in-detect-readelf-with-AC_CHECK_TARGET_TOO.patch) but do not
 # use autoreconf, this does not work on every system correctly. Even makefiles
 # do it that way!
+chmod +w configure
 autoconf configure.in > configure
 
