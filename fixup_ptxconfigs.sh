e899e435ca92260de126a6090399af003923f31a
Michael Olbrich <m.olbrich@pengutronix.de>
2013-12-04 07:45:58 +0100

    fixup_ptxconfigs.sh: add generic arm-v7a-linux-gnueabi toolchains
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -148,6 +148,16 @@ fixup()
 	    ;;
 
 	# softfp, neon
+	arm-v7a-linux-gnueabi)
+	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-float=softfp	--with-fpu=vfpv3-d16			--with-arch=armv7-a"
+	    ;;
+
+	# hard, neon
+	arm-v7a-linux-gnueabihf)
+	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-float=hard	--with-fpu=vfpv3-d16			--with-arch=armv7-a"
+	    ;;
+
+	# softfp, neon
 	arm-cortexa8-linux-gnueabi)
 	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-float=softfp	--with-fpu=neon	--with-cpu=cortex-a8	--with-arch=armv7-a"
 	    ;;
