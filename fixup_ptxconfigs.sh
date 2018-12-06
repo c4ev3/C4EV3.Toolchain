5e3e778b303c3b9e341843700eaa94153f25cc7f
Michael Olbrich <m.olbrich@pengutronix.de>
2018-12-06 15:54:03 +0100

    fixup_ptxconfigs.sh: switch to ptxdist-2018.12.0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -19,7 +19,7 @@ fixup()
     local config="${1}"
 
     # version
-    PTXCONF_CONFIGFILE_VERSION="2018.02.0"
+    PTXCONF_CONFIGFILE_VERSION="2018.12.0"
     PTXCONF_PROJECT="OSELAS.Toolchain-2018.02.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
