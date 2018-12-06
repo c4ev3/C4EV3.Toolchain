161a4f3f88a3f2d9172ee5a6086ad57602003a5f
Michael Olbrich <m.olbrich@pengutronix.de>
2018-12-06 15:58:55 +0100

    release 2018.12.0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -20,7 +20,7 @@ fixup()
 
     # version
     PTXCONF_CONFIGFILE_VERSION="2018.12.0"
-    PTXCONF_PROJECT="OSELAS.Toolchain-2018.02.0"
+    PTXCONF_PROJECT="OSELAS.Toolchain-2018.12.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
     # defaults
